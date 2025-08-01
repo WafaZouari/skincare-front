4e21bc6ffa3c96e516bdd8e86f76aa54ef0f7b74
WafaZouari <wafa.zouari@nehos-groupe.com>
2025-08-01 17:37:10 +0200

    fixing deploying problem

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone", // Required for Render.com
+  experimental: {
+    appDir: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
 
