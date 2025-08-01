f2bc268beb8ff644c5a99d9e8e313b84d134b24a
WafaZouari <wafa.zouari@nehos-groupe.com>
2025-08-01 18:41:56 +0200

    Build

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true, // Nécessaire si tu utilises le dossier /app
-    },
-    output: 'export', // Important pour générer un site statique
 };
 
 module.exports = nextConfig;
